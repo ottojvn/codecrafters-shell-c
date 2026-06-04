2a78cc31e853e5f0f4f23053dd84f277d6ad2050
Joao Victor Otto <ottojvn@tuta.io>
2026-06-04 01:29:27 -0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-  constexpr int command_size = 1024;
   // Flush after every printf
   setbuf(stdout, NULL);
 
   printf("$ ");
-  char command[command_size];
-  constexpr int len = fgets(command, command_size, stdin);
-  command[len] = '\0';
+  char command[BUFSIZ];
+  fgets(command, BUFSIZ, stdin);
+  strtok(command, "\n");
 
   printf("%s: command not found\n", command);
 
