ce09d2690df8a04a6ba272b3ec5fbe9f008d3dfd
Joao Victor Otto <ottojvn@tuta.io>
2026-06-04 01:53:27 -0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,16 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 
+int handle_command(const char *command);
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  printf("$ ");
-  char command[BUFSIZ];
-  fgets(command, BUFSIZ, stdin);
-  strtok(command, "\n");
-
-  printf("%s: command not found\n", command);
+  while (true) {
+    printf("$ ");
+    char command[BUFSIZ];
+    fgets(command, BUFSIZ, stdin);
+    strtok(command, "\n");
+    int ret;
+    if ((ret = handle_command(command)) == -1) {
+      printf("%s: command not found\n", command);
+    } else if (ret == 1) {
+      break;
+    }
+  }
 
   return 0;
+}
+
+int handle_command(const char *command) {
+  if (strcmp(command, "exit") == 0) {
+    return 1;
+  }
+  return -1;
 }
