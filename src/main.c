c841bd06597b63accb7d86f071b877a1e7eb37d8
Joao Victor Otto <ottojvn@tuta.io>
2026-06-04 01:24:03 -0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+  constexpr int command_size = 1024;
   // Flush after every printf
   setbuf(stdout, NULL);
 
   printf("$ ");
+  char command[command_size];
+  fgets(command, command_size, stdin)
 
-  return 0;
+      return 0;
 }
